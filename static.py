58b0e53191ea7758c33b0329becadd95f2da3e43
Jimmy11345 <140876136+Jimmy11345@users.noreply.github.com>
2025-04-13 14:07:39 +0800

    Update static.py

--- static.py
+++ static.py
@@ -10,8 +10,14 @@ url = "https://www.ptt.cc/bbs/miHoYo/index.html"
 # 解決 PTT 年齡驗證與防爬蟲
 cookies = {'over18': '1'}
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'Referer': 'https://www.google.com/',
+    'Accept-Language': 'zh-TW,zh;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Connection': 'keep-alive'
 }
+
 
 response = requests.get(url, cookies=cookies, headers=headers)
 
